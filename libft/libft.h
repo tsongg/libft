d33fb82afbc260966b7d081a84f98a194fe87700
Taeho Song <tsong@c2r4s2.42seoul.kr>
2022-03-13 20:22:16 +0900

    3_13

--- libft/libft.h
+++ libft/libft.h
@@ -24,7 +24,6 @@ void	ft_bzero(void *b, size_t n);
 int		ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlcat(char *dest, const char *src, size_t dstsize);
-
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 
