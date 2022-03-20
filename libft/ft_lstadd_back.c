fdff1a47972c3900bed7ec08eec4b2690d215291
Taeho Song <tsong@c2r4s4.42seoul.kr>
2022-03-20 19:34:02 +0900

    1

--- libft/ft_lstadd_back.c
+++ libft/ft_lstadd_back.c
@@ -1,12 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tsong <tsong@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/20 17:44:35 by tsong             #+#    #+#             */
-/*   Updated: 2022/03/20 17:44:36 by tsong            ###   ########seoul.kr  */
+/*   Created: 2022/03/20 17:44:49 by tsong             #+#    #+#             */
+/*   Updated: 2022/03/20 19:33:22 by tsong            ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	if (!lst || !new)
+		return ;
+	if (*lst == 0)
+		*lst = new;
+	else
+		ft_lstlast(*lst)->next = new;
+}
--- libft/ft_lstlast.c
+++ libft/ft_lstlast.c
@@ -1,12 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tsong <tsong@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/20 17:44:49 by tsong             #+#    #+#             */
-/*   Updated: 2022/03/20 17:44:50 by tsong            ###   ########seoul.kr  */
+/*   Created: 2022/03/20 17:44:35 by tsong             #+#    #+#             */
+/*   Updated: 2022/03/20 19:28:09 by tsong            ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	if (!lst)
+		return (0);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
+}
