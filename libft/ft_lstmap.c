0837a1d0feae3b212a66c24d080f6dbca263472c
Taeho Song <tsong@c2r3s3.42seoul.kr>
2022-03-21 13:32:34 +0900

    final

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: tsong <tsong@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/20 17:45:27 by tsong             #+#    #+#             */
-/*   Updated: 2022/03/20 21:33:11 by tsong            ###   ########seoul.kr  */
+/*   Updated: 2022/03/21 13:03:46 by tsong            ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,24 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*result;
-	t_list	*temp_result;
+	t_list	*temp;
 
 	if (!lst || !f)
 		return (0);
 	result = ft_lstnew(f(lst->content));
 	if (!result)
 		return (0);
-	temp_result = result;
-	lst = lst->next;
-	while (lst)
+	temp = result;
+	while (lst->next)
 	{
-		temp_result->next = ft_lstnew(f(lst->content));
-		if (!temp_result->next)
+		lst = lst->next;
+		temp->next = ft_lstnew(f(lst->content));
+		temp = temp->next;
+		if (!temp)
 		{
 			ft_lstclear(&result, del);
 			return (0);
 		}
-		temp_result = temp_result->next;
-		lst = lst->next;
 	}
 	return (result);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -16,7 +16,6 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memchr(const void *b, int c, size_t n);
 int		ft_memcmp(const void *b1, const void *b2, size_t n);
-
 int		ft_atoi(const char *str);
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
@@ -25,7 +24,6 @@ int		ft_isdigit(int c);
 int		ft_isprint(int c);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
-
 void	ft_bzero(void *b, size_t n);
 int		ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
@@ -36,10 +34,8 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *str);
-
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
-
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
@@ -47,12 +43,10 @@ char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
-
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
